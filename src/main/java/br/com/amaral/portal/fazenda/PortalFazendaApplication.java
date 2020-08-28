e5fb831d72b0d5558ad1527bedbcf5669f5f016b
rafael <rafael_ftec@hotmail.com>
2020-08-27 21:10:02 -0300

    Projeto inicial

---
+++ src/main/java/br/com/amaral/portal/fazenda/PortalFazendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.amaral.portal.fazenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalFazendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalFazendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/amaral/portal/fazenda/PortalFazendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amaral.portal.fazenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalFazendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
